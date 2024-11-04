a691eb615dc6651a7f9f84a8fb0167286156e0ab
Kevin Bett <prodigy4614@gmail.com>
2024-11-04 08:13:19 +0300

    update config files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bhsbiodigesterLandingPage/"
+  base: "/bhsbiodigesterLandingPage/",
+  build: {
+    outDir: 'build'  // Set the output directory to 'build'
+  }
 })
